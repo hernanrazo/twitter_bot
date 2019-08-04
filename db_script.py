3f0d22658a713fa3df4575611b5b1e0423c73c57
Hernan Razo <hrazo@hawk.iit.edu>
2019-08-04 18:30:22 -0500

    fixed dependencies and worked on sql queries

--- db_script.py
+++ db_script.py
@@ -1,1 +1,26 @@
+import sqlalchemy
 import db_connect
+
+
+def is_empty(conn):
+
+    check = conn.execute('SELECT SIGN(COUNT(*)) FROM tweets')
+    result = check.fetchall()
+
+    return result
+
+
+def read_query(conn, random_num):
+
+    result = conn.execute("SELECT TWEET FROM TWEETS WHERE id = :num",{'num':random_num})
+    return result
+
+
+def delete_query(conn, random_num):
+
+    result = conn.execute('DELETE tweet FROM tweets WHERE id = :num', {'num': random_num})
+
+
+
+
+
