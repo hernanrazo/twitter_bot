18f0781ddea32d4c4924b8fe846d61a03d4f9a75
Hernan Razo <hrazo@hawk.iit.edu>
2019-08-10 00:07:49 -0500

    almost done redoing queries

--- db_script.py
+++ db_script.py
@@ -1,29 +1,26 @@
 import sqlalchemy
 import db_connect
 
+#check if table is empty. return 1 if yes, 0 if not
+def is_empty(session):
 
-def is_empty(conn):
-
-    check = conn.execute('SELECT SIGN(COUNT(*)) FROM tweets')
+    check = session.execute('SELECT SIGN(COUNT(*)) FROM tweets')
     result = check.fetchall()
-
     return result
 
 
-def read_query(conn, random_num):
+#get tweet that matches the given id
+def read_query(session, random_num):
 
-    result = conn.execute("SELECT TWEET FROM TWEETS WHERE id = :num",{'num':random_num})
-
+    result = session.execute('SELECT TWEET FROM TWEETS WHERE id = :num',{'num':random_num})
     query = result.fetchall()
-
     return query
 
 
-def delete_query(conn, random_num):
+#delete the tweet that matches the inputted string
+def delete_query(session, tweet):
 
-    result = conn.execute('DELETE tweet FROM tweets WHERE id = :num', {'num': random_num})
-
-
-
+    delete_query = session.query(tweets_table).filter_by(tweets_table.tweets=tweet)
+    delete_query.delete(synchronize session='evaluate')
 
 
