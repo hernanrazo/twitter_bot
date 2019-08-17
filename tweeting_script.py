b301d60a1fac5a4d5f1c7e99d38eccf2640aa2a7
Hernan Razo <hrazo@hawk.iit.edu>
2019-08-16 19:26:59 -0500

    added tweet deletion functionality

--- tweeting_script.py
+++ tweeting_script.py
@@ -59,7 +59,7 @@ def tweet_pipeline(api):
 
         tweet = get_tweet(my_cursor)
         post_tweet(tweet, api)
-#        db_script.delete_query(my_cursor, tweet)
+        db_script.delete_query(my_cursor, tweet)
         my_cursor.close()
         conn.close()
         print('Waiting for next tweet...')
