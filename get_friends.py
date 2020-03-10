2c621d4fe73ce6726cb6f5907caf2bb3de2e4ec7
Hernan Razo <hrazo@hawk.iit.edu>
2020-03-10 10:33:59 -0500

    moved screen name to heroku config vars

--- get_friends.py
+++ get_friends.py
@@ -1,12 +1,12 @@
 import tweepy
 import time
-
+import os
 
 #compile list of currently followed users 
 def get_friends(api):
-
+    SCREEN_NAME = os.environ['SCREEN_NAME']
     friends_list = []
-    friends = tweepy.Cursor(api.friends, screen_name='WNUTSHANG').items()
+    friends = tweepy.Cursor(api.friends, screen_name=SCREEN_NAME).items()
     for user in friends:
         friends_list.append(user.screen_name)
     return friends_list
